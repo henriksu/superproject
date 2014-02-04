66aece8895a69f979b470d05c00cb974a6b80c3f
Henrik Sperre Sundklakk <henrik.sundklakk@gmail.com>
2014-02-04 17:30:30 +0100

    New edition of oppg1.c
    cleaned up oppg1.c so that the vector is created for each Sn. I think this will
    make it easier to compare problems of different sizes.
    
    I also implemented two different ways of summing the vector, one numerically
    bad and one numerically optimal. The difference was very small.

---
+++ code/oppg1.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double sum(int N);
+double sumSlow(int N);
+
+int main(int argc,char** argv){
+	double S = (M_PI*M_PI)/6;
+	int iterations = 14, i;
+	int N[iterations];
+	
+	
+	N[0] = 8;
+	for(i=1; i<iterations; ++i)
+	{
+		N[i] = 2*N[i-1];
+	}
+	double* Sn = (double*)malloc(iterations*sizeof(double));
+	double* SnSlow = (double*)malloc(iterations*sizeof(double));
+	
+	for(i=0; i<iterations; ++i)
+	{
+		Sn[i] = sum(N[i]);
+		SnSlow[i] = sumSlow(N[i]);
+	}
+	for(i=0; i<iterations; ++i)
+	{
+		printf("%e %e\n",S-SnSlow[i], Sn[i]-SnSlow[i]);
+	}
+	free(Sn); free(SnSlow);
+	return 0;
+}
+
+// Makes and sums up a vector. Sums in the numerically worst possible way.
+double
+sum(int N)
+{
+	int i;
+	double Sn=0;
+	double* v = (double*)malloc(N*sizeof(double));
+	for(i=1;i<=N;++i)
+	{
+		v[i-1] = 1.0/(i*i);
+	}
+	for(i=0; i<N; ++i)
+	{
+		Sn += v[i];
+	}
+	return Sn;
+}
+
+// Slower edition of sum. Sums by always adding the two smallest numbers. Numarically optimal.
+double
+sumSlow(int N)
+{
+	int i, j;
+	double* v = (double*)malloc(N*sizeof(double));
+	double tmp;
+	for(i=1;i<=N;++i)
+	{
+		v[i-1] = 1.0/(i*i);
+	}
+	for(i=N-1; i>=1; --i)
+	{
+		v[i-1] += v[i];
+		j=i-1;
+		while(v[j]>v[j-1] && j>0)
+		{
+			tmp = v[j-1];
+			v[j-1] = v[j];
+			v[j] = tmp;
+			--j;
+		}
+	}
+	tmp = v[0];
+	free(v);
+	return tmp;
+}
+
