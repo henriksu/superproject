63b3103fc9eb188b433b3223c1803dfd4b7e471f
Henrik Sperre Sundklakk <henrik.sundklakk@gmail.com>
2014-02-10 10:14:33 +0100

    All the calculation schemes work.
    Added the combined MPI/OpenMP from Erlend.

--- code/oppg1.c
+++ code/oppg1.c
@@ -3,14 +3,19 @@
 #include <stdlib.h>
 
 #include "common.h"
+//#include "sum.h"
 
 double sum(const int N);
 double sumSlow(const int N);
 double sumShared(const int N);
-double sumDist(const int N, int* rank, int argc, char** argv);
+double sumDist(const int N, int* rank);
+double sumHybrid(const int N, int* rank);
 
 double* initVec_seq(const int N);
 double* initVec_shared(const int N);
+double* initVec_dist(const int N_part, const int* rank, const int size);
+double* initVec_hybrid(const int N_part, const int* rank, const int size);
+
 double summingSeq(double* v, const int N);
 double summingSlow(double* v, const int N);
 double summingShared(double* v, const int N);
@@ -20,25 +25,25 @@ void basicSetup(const int iterations, int N[], double* Sn, double* SnSlow); // F
 
 
 int main(int argc,char** argv){
-	int i, rank = 0; // Generic loop variable.
+	int rank = 0;
+	int i; // Generic loop variable.
 	double startTime; // Storing the start time while measuring.
 	double S = (M_PI*M_PI)/6; // The limit of the series.
-
+	MPI_Init(&argc,&argv);
 
 	// Setting up the size of the partial sums to generate. This should be altered to read something from the command line.
-	int iterations = 14; // Number of different summing lengths.
+	int iterations = 13; // Number of different summing lengths.
 	int N[iterations]; // Vector with the summetion lengths.
 	double* Sn     = (double*)malloc(iterations*sizeof(double));
 	double* SnSlow = (double*)malloc(iterations*sizeof(double)); // Vectors of the partial sums.
 	basicSetup(iterations, N, Sn, SnSlow);
 
-	MPI_Init(&argc,&argv);	
+	
 	for(i=0; i<iterations; ++i)
 	{
-		Sn[i] = sumDist(N[i], &rank, argc, argv);
+		Sn[i] = sum(N[i]);//, &rank);
 		SnSlow[i] = sumSlow(N[i]);
 	}
-	MPI_Finalize();
 
 	if(rank==0)
 		for(i=0; i<iterations; ++i)
@@ -46,6 +51,8 @@ int main(int argc,char** argv){
 			printf("%e %e\n",S-SnSlow[i], Sn[i]-SnSlow[i]);
 		}
 	free(Sn); free(SnSlow);
+	
+	MPI_Finalize();
 	return 0;
 }
 
@@ -91,7 +98,7 @@ sumShared(const int N)
 }
 
 double
-sumDist(const int N, int* rank, int argc, char** argv)
+sumDist(const int N, int* rank)
 {
 	double Sn=0;
 	double Sn_temp;
@@ -101,27 +108,36 @@ sumDist(const int N, int* rank, int argc, char** argv)
 
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD,rank);
-	MPI_Status Status;
 
-	//The following is OK only as long as N = 2^k1 and P=2^k2 and k1>k2.
+	//The following line is OK only as long as N = 2^k1 and P=2^k2 and k1>k2.
 	N_part = N/size;
-	v = (double*)malloc(N_part*sizeof(double));
-	if(*rank == 0)
-	{
-		int dest,j;		
-		for(dest=1;dest<=size;++dest)// The last iteration is not sent to rank==size, but stays in rank==0.
-		{
-			int move = (dest-1)*N_part+1;
-			for(j=0;j<N_part;++j)
-				v[j] = 1.0/((j+move)*(j+move));
-			if(dest < size)
-				MPI_Send(v,N_part,MPI_DOUBLE,dest,100,MPI_COMM_WORLD);
-		}
-	}else{
-		MPI_Recv(v,N_part,MPI_DOUBLE,0,100,MPI_COMM_WORLD,&Status);
-	}
-
+	v = initVec_dist(N_part, rank, size);
 	Sn_temp = summingSeq(v,N_part);
+	free(v);
+	
+	MPI_Reduce(&Sn_temp,&Sn,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);	
+	if(*rank == 0)
+		return Sn;
+	else
+		return 0.0;
+}
+
+double
+sumHybrid(const int N, int* rank)
+{
+	double Sn=0;
+	double Sn_temp;
+	int size;
+	int N_part;
+	double* v;
+
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,rank);
+
+	//The following line is OK only as long as N = 2^k1 and P=2^k2 and k1>k2.
+	N_part = N/size;
+	v = initVec_hybrid(N_part, rank, size);
+	Sn_temp = summingShared(v,N_part);
 	free(v);
 	
 	MPI_Reduce(&Sn_temp,&Sn,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);	
@@ -153,6 +169,50 @@ double* initVec_shared(const int N)
 	}
 	return v;
 }
+
+double* initVec_dist(const int N_part, const int* rank, const int size)
+{
+	MPI_Status Status;
+	double* v = (double*)malloc(N_part*sizeof(double));
+	if(*rank == 0)
+	{
+		int dest,j,move;		
+		for(dest=1; dest<=size; ++dest) // The last iteration is not sent to rank==size, but stays in rank==0.
+		{
+			move = (dest-1)*N_part+1;
+			for(j=0;j<N_part;++j)
+				v[j] = 1.0/((j+move)*(j+move));
+			if(dest < size)
+				MPI_Send(v,N_part,MPI_DOUBLE,dest,100,MPI_COMM_WORLD);
+		}
+	}else{
+		MPI_Recv(v,N_part,MPI_DOUBLE,0,100,MPI_COMM_WORLD,&Status);
+	}
+	return v;
+}
+
+double* initVec_hybrid(const int N_part, const int* rank, const int size)
+{
+	MPI_Status Status;
+	double* v =  (double*)malloc(N_part*sizeof(double));
+	if(*rank == 0)
+	{
+		int dest,j, move = 1;
+		for(dest=1; dest<=size; ++dest) // The last iteration is not sent to rank==size, but stays in rank==0.
+		{
+			#pragma parallel for schedule(Static)
+			for(j=0;j<N_part;++j)
+				v[j] = 1.0/((j+move)*(j+move));
+			if(dest < size)
+				MPI_Send(v,N_part,MPI_DOUBLE,dest,100,MPI_COMM_WORLD);
+			move += N_part;
+		}
+	}else{
+		MPI_Recv(v,N_part,MPI_DOUBLE,0,100,MPI_COMM_WORLD,&Status);
+	}
+	return v;
+}
+
 
 double summingSeq(double* v, const int N)
 {
