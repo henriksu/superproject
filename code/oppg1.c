82f39e82ce0257950c63aba9f058d14b58887145
Henrik Sperre Sundklakk <henrik.sundklakk@gmail.com>
2014-02-11 17:04:47 +0100

    streamlined the program for the command line.
    Used Johannes file oppg1_withinput.c to make oppg1.c produce a binary that
    parses command line input and produces output on a standardized format.

--- code/oppg1.c
+++ code/oppg1.c
@@ -3,53 +3,81 @@
 #include <stdlib.h> // malloc.
 
 #include "sum.h" // All our functions + common.h ( MPI, omp, WallTime() ).
-void basicSetup(const int iterations, int N[], double* Sn, double* SnSlow); // Fills N[] with "iterations" powers of 2, starting at 8.
 
 int main(int argc,char** argv){
-	int rank = 0;
 	int i; // Generic loop variable.
-	double startTime; // Storing the start time while measuring.
+	int N; // Numer of elements to sum.
+	int method; // Indicates what summing medthod to use. 1=task 1, 2=task 2, 3=task 3, 4=task 4, 5=slowSum.
+	int rank = 0; // MPI rank.
+	double time; // Used to store both start time (seconds since 1970?) and time deltas in seconds.
 	double S = (M_PI*M_PI)/6; // The limit of the series.
+	double Sn; // Storing the partial sum.
+
 	MPI_Init(&argc,&argv);
 
-	// Setting up the size of the partial sums to generate. This should be altered to read something from the command line.
-	int iterations = 13; // Number of different summing lengths.
-	int N[iterations]; // Vector with the summetion lengths.
-	double* Sn     = (double*)malloc(iterations*sizeof(double));
-	double* SnSlow = (double*)malloc(iterations*sizeof(double)); // Vectors of the partial sums.
-	basicSetup(iterations, N, Sn, SnSlow);
-
-	
-	for(i=0; i<iterations; ++i)
+	// Parse commandline input.
+	if(argc >= 2)
 	{
-		Sn[i] = sum(N[i]);//, &rank);
-		SnSlow[i] = sumSlow(N[i]);
+		if (atoi(argv[1])>0 && atoi(argv[1])<6)
+			method = atoi(argv[1]);
+		else // Default
+			method = 5;
+		N = atoi(argv[2]);
+	}
+	else if (argc >=1)
+	{
+		method = 5;
+		N = atoi(argv[1]);
+	}		
+
+	// Do calculations.
+	switch (method)
+	{
+		case 1:
+			printf("Running the non-parallelized programm (Task1).\n");
+			time  = WallTime();
+			Sn = sum(N);
+			time = WallTime() - time;
+			break;
+		case 2:
+			printf("Running the openMP-parallelized programm (Task2).\n");
+			time= WallTime();
+			Sn = sumShared(N);
+			time = WallTime() - time;
+			break;
+		case 3:
+			MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+			if(rank==0)			
+				printf("Running the MPI-parallelized programm (Task3).\n");
+			time = WallTime();
+			Sn = sumDist(N,&rank);
+			time = time - WallTime();
+			break;
+		case 4:
+			MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+			if(rank==0)
+				printf("Running the openMP- and MPI-parallelized programm (Task4).\n");
+			time = WallTime();
+			Sn = sumHybrid(N,&rank);
+			time = time - WallTime();
+			break;
+		case 5:
+			printf("Running the non-parallelized programm with better summation order(Task1).\n");
+			time= WallTime();
+			Sn = sumSlow(N);
+			time = time - WallTime();
+			break;
 	}
 
+	// Output results.
 	if(rank==0)
-		for(i=0; i<iterations; ++i)
-		{
-			printf("%e %e\n",S-SnSlow[i], Sn[i]-SnSlow[i]);
-		}
-	free(Sn); free(SnSlow);
+	{
+		printf("n \terror \t\ttime\n");
+		printf("%d \t%e \t%e\n", N, S-Sn, time);
+	}
 	
+	// Clean up.
 	MPI_Finalize();
 	return 0;
 }
-
-void
-basicSetup(const int iterations, int *N, double* Sn, double* SnSlow)
-{
-	int i;
-	N[0] = 8;
-	for(i=1; i<iterations; ++i)
-	{
-		N[i] = 2*N[i-1];
-	}
-	return;
-}
-
-
-
-
 
