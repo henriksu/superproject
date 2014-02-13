f5674b8ba4e757a77bc2cf11d13073c47cbf6d7d
Henrik Sperre Sundklakk <henrik.sundklakk@gmail.com>
2014-02-13 08:26:07 +0100

    Corrected !# to #!.

--- code/kongul.sh
+++ code/kongul.sh
@@ -1,29 +1,32 @@
-!#/bin/bash
+#!/bin/bash
 
 # The PBS lines are for the que system.
 #PBS -N test_kongull
 # "default" refers to the kind of CPU. alternative is "intel". The ppn refers to _MPI_ processes per node.
 #PBS -lnodes=4:ppn=12:default
-# Max time. Kill afterwards
+# Max time. Set it as low as possible to get priority. But remember that the job is killed at timeout.
 #PBS -lwalltime=00:00:30
-# Memory per MPI process. 24 GB per node available.
+# Memory per MPI process. There is 24 GB available on each node.
 #PBS -lpmem=2000MB
-# Acount (who pays)
+# Account (who pays)
 #PBS -A freecycle
-# que identity.
+# What que to place it in.
 #PBS -q optimist
-# what to do with output. (stdout and stderr in same file)
+# what to do with output (stdout and stderr in same file).
 #PBS -j oe
 
 # This is a bash script (run once on one node)
 cd ${PBS_O_WORKDIR}
 
-# To get access to libraries. Demands the compilators to be loaded.
+# To get access to libraries, the following modules must be loaded.
 module load intelcomp
 module load openmpi/1.4.3-intel
-# How memory is used during multithreading.
+
+# How threads are treated. Something about keeping each thread on one processor to avoid copying cache or something like that.
 KMP_AFFINITY="granularity=fine , compact"
 
+
+# From this point on it is an ordinary bash-script, calling the interesting program.
 for n in `seq 3 14`
 do
 	ELAPSED=`OMP_NUM_THREADS=1 mpirun -npernode 1 ./oppg1 1 $n | grep "^[0-9]"`
